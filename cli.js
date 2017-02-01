fe82db335f2474b090bf35b4406023dbdf5691b2
Perry Mitchell <perry@perrymitchell.net>
2017-02-01 23:48:36 +0200

    Add support for version output

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 
 const path = require("path");
 const argv = require("minimist")(process.argv.slice(2));
+const pkg = require("./package.json");
 
 const Ochre = require("./source/index.js");
 
@@ -15,11 +16,16 @@ Usage: ochre <config|package> [options]
     -d, --dry-run           Log but take no permanent action
     --output                Output destination (filename)
     -q, --quiet             Quiet (yes to all prompts)
+    -v, --version           Output the version
     `);
 }
 
 if (argv.h === true || argv.help === true) {
     showHelp();
+    return;
+}
+if (argv.v === true || argv.version === true) {
+    console.log(pkg.version);
     return;
 }
 
